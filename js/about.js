3294ab0a8cfb24b4b5de08ba53c9be90018e07e0
Vahid819 <vahid.yama@gmail.com>
2024-06-24 18:33:33 +0530

    add loading effect

--- js/about.js
+++ js/about.js
@@ -4,6 +4,20 @@ displaytext.addEventListener("mouseover", ()=>{
     display.toggle("displaytext")
 })
 
+let loder = document.querySelector(".loader");
+window.addEventListener("load", ()=>{
+  let loding = document.querySelector(".loding_effect");
+  loding.style.display = "none";
+  let socail = document.querySelector(".socal")
+  socail.style.display = "flex";
+  let navbar = document.querySelector(".navbar")
+  navbar.style.display = "flex";
+  let contenar = document.querySelector(".maincontenar")
+  contenar.style.display = "block";
+  let footer = document.querySelector(".footers")
+  footer.style.display = "grid"
+})
+
 let hidemenu = document.querySelector(".droplist")
 hidemenu.addEventListener("mouseleave", ()=>{
     hidemenu.classList.remove("displaytext")
