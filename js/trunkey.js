0e7ecf739546fa636bc52e1ce665313c3103d66e
Vahid819 <vahid.yama@gmail.com>
2024-07-06 19:12:54 +0530

    add content and animation

--- js/trunkey.js
+++ js/trunkey.js
@@ -14,6 +14,16 @@ let loder = document.querySelector(".loader");
 window.addEventListener("load", ()=>{
   let loding = document.querySelector(".loding_effect");
   loding.style.display = "none";
+//   let display_content = document.getElementsByTagName("footer");
+//   display_content[0].style.display="grid";
+  let image = document.querySelector(".maincontenar");
+  image.style.display = "block";
   let navbar = document.querySelector(".navbar");
   navbar.style.display = "flex";
+  let service = document.querySelector(".service");
+  service.style.display = "flex";
+//   let service2 = document.querySelector(".service2");
+//   service2.style.display = "flex";
+//   let videos = document.querySelector(".vides");
+//   videos.style.display = "flex";
 })
