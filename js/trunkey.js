e5ed2c4a50dffa921d25bdb9e9a651ecba03dd0e
Vahid819 <vahid.yama@gmail.com>
2024-07-11 17:26:52 +0530

    add animation

--- js/trunkey.js
+++ js/trunkey.js
@@ -27,3 +27,18 @@ window.addEventListener("load", ()=>{
 //   let videos = document.querySelector(".vides");
 //   videos.style.display = "flex";
 })
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); // Change image every 2 seconds
+}
