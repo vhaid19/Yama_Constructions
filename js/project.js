403abfed70725d71b756822b3953b758341117a3
Vahid819 <vahid.yama@gmail.com>
2024-07-25 18:54:59 +0530

    changis on project managment page

--- js/project.js
+++ js/project.js
@@ -1,6 +1,28 @@
-let timer = document.querySelector(".timer");
-setInterval(() => {
-    const time = new Date();
-    // console.log(time.toDateString())
-    timer.innerHTML = time.toLocaleTimeString();
-}, 1000);
+let loder = document.querySelector(".loader");
+window.addEventListener("load", ()=>{
+  let loding = document.querySelector(".loding_effect");
+  loding.style.display = "none";
+  let navbar = document.querySelector(".navbar")
+  navbar.style.display = "flex";
+  let background_content = document.querySelector(".background_contenar")
+  background_content.style.display = "flex";
+})
+
+let menu = document.getElementById("Services")
+menu.addEventListener("mouseover", ()=>{
+  let menus = document.querySelector(".droplist")
+  menus.classList.toggle("displaymenu");
+  console.log("vahid");
+})
+
+let menu2 = document.querySelector(".droplist");
+menu2.addEventListener("mouseleave", ()=>{
+  menu2.classList.remove("displaymenu")
+})
+
+let showmenu = document.querySelector(".menu");
+showmenu.addEventListener("click", ()=>{
+  let nav = document.querySelector(".nav");
+  nav.classList.toggle("navshow");
+  // nav.style.display="flex"
+})
