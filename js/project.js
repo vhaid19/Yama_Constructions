5286be7ad38f9220dfe83e0da38eb2ab8e2ec522
Vahid819 <vahid.yama@gmail.com>
2024-08-07 18:33:50 +0530

    add animation using javascript

--- js/project.js
+++ js/project.js
@@ -7,7 +7,6 @@ window.addEventListener("load", ()=>{
   let background_content = document.querySelector(".background_contenar")
   background_content.style.display = "flex";
 })
-
 let menu = document.getElementById("Services")
 menu.addEventListener("mouseover", ()=>{
   let menus = document.querySelector(".droplist")
@@ -26,3 +25,39 @@ showmenu.addEventListener("click", ()=>{
   nav.classList.toggle("navshow");
   // nav.style.display="flex"
 })
+
+
+window.addEventListener("load", ()=>{
+document.querySelector(".navbar").animate(drop_down, dropdown_time);  
+document.querySelector(".nav").animate(drop_down, dropdown_time)
+document.querySelector("h1").animate(slide_contenar,dropdown_time)
+document.querySelector(".managment_info ").animate(slideright_contenar, dropdown_time)
+document.querySelector("img ").animate(scaleimage, dropdown_time).style.opacity = "1"
+
+})
+
+const dropdown_time = {
+  duration: 1200,
+   iterations: 1,
+}
+
+const drop_down = [
+  {transform: "translateY(-10ch)"},
+  {transform: "translateY(0ch)"}
+]
+
+const slide_contenar = [
+  {transform: "translateX(-100%) scale(0)"},
+  {transform: "translateX(0%) scale(1)"}
+]
+
+const slideright_contenar = [
+  {transform: "translateX(100%) scale(0)"},
+  {transform: "translateX(0%) scale(1)"}
+]
+
+const scaleimage = [
+  {transform: "scale(0)"},
+  {transform: "scale(1)"},
+]
+
