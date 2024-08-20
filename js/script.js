7eb2a90766f37f96640e8f860252faf048ca850e
Vahid819 <vahid.yama@gmail.com>
2024-08-20 18:27:54 +0530

    responsiveness completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 let menu = document.querySelector(".menulogo");
-menu.addEventListener("click", ()=>{
-    let menus = document.querySelector(".nav2").classList;
-    menus.toggle("displaymenu");
-    console.log("Vahid")
-    let hide = document.querySelector(".main").classList;
-    hide.toggle("hidemain");
-    let hides = document.querySelector(".footer").classList;
-    hides.toggle("hidefooter");
-
-})
+menu.addEventListener("click", display_menu)
+function display_menu(){
+  let menus = document.querySelector(".nav2").classList;
+  menus.toggle("displaymenu");
+  // console.log("Vahid")
+  let hide = document.querySelector(".main").classList;
+  hide.toggle("hidemain");
+  let contenar = document.querySelector(".main")
+  contenar.classList.toggle("show_not")
+  let hides = document.querySelector(".footer").classList;
+  hides.toggle("hidefooter");
+}
 
 let loder = document.querySelector(".loader");
 window.addEventListener("load", ()=>{
@@ -17,21 +19,25 @@ window.addEventListener("load", ()=>{
   let navbar = document.querySelector(".navbar")
   navbar.style.display = "flex";
   let contenar = document.querySelector(".main")
-  contenar.style.display = "block";
+  contenar.classList.toggle("show_not")
   let footer = document.querySelector(".footer")
   footer.style.display = "flex";
 })
 
 let menus = document.querySelector(".services");
-menus.addEventListener("mouseover", ()=>{
-    let menuss = document.querySelector(".menus").classList;
+menus.addEventListener("mouseover", menu_over)
+
+function menu_over(){
+  let menuss = document.querySelector(".menus").classList;
     menuss.toggle("showmenus")
-})
+}
 
 let submenus = document.querySelector(".menus");
-submenus.addEventListener("mouseleave", ()=>{
-    submenus.classList.remove("showmenus")
-})
+submenus.addEventListener("mouseleave", menu_leave)
+
+function menu_leave(){
+  submenus.classList.remove("showmenus")
+}
 
 let slideIndex = 0;
 showSlides();
@@ -47,3 +53,19 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3500); // Change image every 2 seconds
 }
+
+
+let width = window.innerWidth;
+// console.log(typeof(width))
+if(width <= 430){
+  menus.removeEventListener("mouseover", menu_over)
+  submenus.removeEventListener("mouseleave", menu_leave)
+  menus.addEventListener("click", menu_click)
+  console.log("vahid")
+}
+
+function menu_click(){
+  document.querySelector(".menus").classList.toggle("showmenus");
+  console.log("vahid")
+}
+
